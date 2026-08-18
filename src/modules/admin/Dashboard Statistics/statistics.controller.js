fa2aa36e925e7b4c3b0c594a6189a93cfe894df9
Hadeer Mostafa <Hadeer.Mostafa00@outlook.com>
2026-08-18 09:01:16 +0300

    feat: add recent contact messages endpoint and refactor dashboard statistics retrieval

--- src/modules/admin/Dashboard Statistics/statistics.controller.js
+++ src/modules/admin/Dashboard Statistics/statistics.controller.js
@@ -4,30 +4,37 @@ import { Projects } from "../../../../DB/models/admin/project.model.js";
 import { ContactMessage } from "../../../../DB/models/admin/contactUs.model.js";
 import { sendSuccess } from "../../../utils/successResponse.js";
 
-export const getDashboardStatistics = catchError (async (req , res , next) => {
-   const [
-    totalProducts,
-    totalProjects,
-    totalMessages,
-    unreadMessages
-  ] = await Promise.all([
-    Products.countDocuments(),
-    Projects.countDocuments(),
-    ContactMessage.countDocuments({ isDeleted: false }),
-    ContactMessage.countDocuments({ isRead: false, isDeleted: false })
-  ]);
+export const getDashboardStatistics = catchError(async (req, res, next) => {
+  const [totalProducts, totalProjects, totalMessages, unreadMessages] =
+    await Promise.all([
+      Products.countDocuments(),
+      Projects.countDocuments(),
+      ContactMessage.countDocuments({ isDeleted: false }),
+      ContactMessage.countDocuments({ isRead: false, isDeleted: false }),
+    ]);
 
-  return sendSuccess(
-    res,
-    200,
-    "Dashboard statistics retrieved successfully",
-    {
-      summary: {
-        totalProducts,
-        totalProjects,
-        totalMessages,
-        unreadMessages
-      }
-    }
-  )
-})
+  return sendSuccess(res, 200, "Dashboard statistics retrieved successfully", {
+    summary: {
+      totalProducts,
+      totalProjects,
+      totalMessages,
+      unreadMessages,
+    },
+  });
+});
+
+export const recentContactMessages = catchError(async (req, res, next) => {
+  const [latestMessages, totalCount, unreadCount] = await Promise.all([
+    ContactMessage.find()
+      .sort({ createdAt: -1 })
+      .limit(5)
+      .select("-__v -updatedAt -isDeleted -readAt -phoneNumber -message"),
+    ContactMessage.countDocuments(),
+    ContactMessage.countDocuments({ isRead: false }),
+  ]);
+  return sendSuccess(res, 200, "Recent messages retrieved successfully", {
+    messages: [latestMessages],
+    totalCount,
+    unreadCount,
+  });
+});
--- src/modules/admin/Dashboard Statistics/statistics.router.js
+++ src/modules/admin/Dashboard Statistics/statistics.router.js
@@ -1,9 +1,15 @@
 import { Router } from "express";
 import { isAuthenticated } from "../../../middleware/isAuth.js";
-import { getDashboardStatistics } from "./statistics.controller.js";
+import {
+  getDashboardStatistics,
+  recentContactMessages,
+} from "./statistics.controller.js";
 
 const router = Router();
 
 //Get Dashboard Statistics
-router.get("/statistics",isAuthenticated,getDashboardStatistics)
-export default router
+router.get("/statistics", isAuthenticated, getDashboardStatistics);
+// Get Recent Contact Messages
+router.get("/recent-messages", isAuthenticated, recentContactMessages);
+
+export default router;
